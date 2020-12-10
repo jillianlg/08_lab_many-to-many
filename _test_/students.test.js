2da6ada55867daf605268b0c6ba2d5686b54a0fa
Jillian Gibson <jillian.l.gibson@gmail.com>
2020-12-10 14:36:04 -0800

    Adds GET by ID route for Student class with passing test

--- _test_/students.test.js
+++ _test_/students.test.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const request = require('supertest');
 const app = require('../lib/app');
+const Student = require('../lib/models/Student');
 const pool = require('../lib/utils/pool');
     
 describe('students routes', () => {
@@ -26,6 +27,18 @@ describe('students routes', () => {
       name: 'Jamal',
       email: 'jamal@email.com',
     });
+  });
+
+  it('finds a student by id via GET', async() => {
+    const student = await Student.insert({
+      name: 'Jamal',
+      email: 'jamal@email.com',
+    });
+
+    const res = await request(app)
+      .get(`/api/v1/students/${student.id}`);
+
+    expect(res.body).toEqual(student);
   });
 
 });
